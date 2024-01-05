496ecf90851b8a25f0b4fd7cfe8d609cf663b47f
ryderwishart <ryderwishart@gmail.com>
2024-01-05 13:37:19 -0800

    Fresh start

--- src/extension/inputController.ts
+++
@@ -1,7 +0,0 @@
-// This code is for handling the cell inputs, such as auto-completions
-
-import { DEBUG_MODE, NAME, MIME_TYPE, LABEL, LANGUAGE, DESCRIPTION } from '../common/common';
-import * as vscode from 'vscode';
-import * as fs from 'fs';
-import * as path from 'path';
-import { exec } from 'child_process';
