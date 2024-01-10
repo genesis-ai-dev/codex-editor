124f1fd72957f0f65b769fabf216454f68c8c26d
Ben Scholtens <ben.scholtens@me.com>
2024-01-10 15:03:17 -0500

    Add optional meta data to code cells

--- src/serializer.ts
+++ src/serializer.ts
@@ -1,5 +1,5 @@
-import * as vscode from 'vscode';
-import { TextDecoder, TextEncoder } from 'util';
+import * as vscode from "vscode";
+import { TextDecoder, TextEncoder } from "util";
 
 /**
  * An ultra-minimal sample provider that lets the user type in JSON, and then
@@ -7,52 +7,66 @@ import { TextDecoder, TextEncoder } from 'util';
  */
 
 interface RawNotebookData {
-	cells: RawNotebookCell[]
+    cells: RawNotebookCell[];
 }
 
 interface RawNotebookCell {
-	language: string;
-	value: string;
-	kind: vscode.NotebookCellKind;
-	editable?: boolean;
+    language: string;
+    value: string;
+    kind: vscode.NotebookCellKind;
+    editable?: boolean;
+    metadata?: any;
 }
 
 export class SampleContentSerializer implements vscode.NotebookSerializer {
-	public readonly label: string = 'My Sample Content Serializer';
+    public readonly label: string = "My Sample Content Serializer";
 
-	public async deserializeNotebook(data: Uint8Array, token: vscode.CancellationToken): Promise<vscode.NotebookData> {
-		const contents = new TextDecoder().decode(data); // convert to String
+    public async deserializeNotebook(
+        data: Uint8Array,
+        token: vscode.CancellationToken,
+    ): Promise<vscode.NotebookData> {
+        const contents = new TextDecoder().decode(data); // convert to String
 
-		// Read file contents
-		let raw: RawNotebookData;
-		try {
-			raw = <RawNotebookData>JSON.parse(contents);
-		} catch {
-			raw = { cells: [] };
-		}
+        // Read file contents
+        let raw: RawNotebookData;
+        try {
+            raw = <RawNotebookData>JSON.parse(contents);
+        } catch {
+            raw = { cells: [] };
+        }
+        // Create array of Notebook cells for the VS Code API from file contents
+        const cells = raw.cells.map(
+            (item) =>
+                new vscode.NotebookCellData(
+                    item.kind,
+                    item.value,
+                    item.language,
+                ),
+        );
 
-		// Create array of Notebook cells for the VS Code API from file contents
-		const cells = raw.cells.map(item => new vscode.NotebookCellData(
-			item.kind,
-			item.value,
-			item.language
-		));
+        const mewCells = cells.map((cell, index) => {
+            cell.metadata = raw.cells[index].metadata;
+            return cell;
+        });
+        return new vscode.NotebookData(mewCells);
+    }
 
-		return new vscode.NotebookData(cells);
-	}
+    public async serializeNotebook(
+        data: vscode.NotebookData,
+        token: vscode.CancellationToken,
+    ): Promise<Uint8Array> {
+        // Map the Notebook data into the format we want to save the Notebook data as
+        const contents: RawNotebookData = { cells: [] };
 
-	public async serializeNotebook(data: vscode.NotebookData, token: vscode.CancellationToken): Promise<Uint8Array> {
-		// Map the Notebook data into the format we want to save the Notebook data as
-		const contents: RawNotebookData = { cells: [] };
+        for (const cell of data.cells) {
+            contents.cells.push({
+                kind: cell.kind,
+                language: cell.languageId,
+                value: cell.value,
+                metadata: cell.metadata,
+            });
+        }
 
-		for (const cell of data.cells) {
-			contents.cells.push({
-				kind: cell.kind,
-				language: cell.languageId,
-				value: cell.value
-			});
-		}
-
-		return new TextEncoder().encode(JSON.stringify(contents));
-	}
+        return new TextEncoder().encode(JSON.stringify(contents));
+    }
 }
