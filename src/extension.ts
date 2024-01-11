510b0204283ca437bfa93e8580b0fef82ca0356c
ryderwishart <ryderwishart@gmail.com>
2024-01-10 16:04:01 -0800

    Minor cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -5,23 +5,23 @@ import { NOTEBOOK_TYPE, createCodexNotebook, createProjectNotebooks } from './co
 
 export function activate(context: vscode.ExtensionContext) {
 
-	context.subscriptions.push(
-		vscode.workspace.registerNotebookSerializer(
+	// Register the Codex Notebook serializer for saving and loading .codex files
+	context.subscriptions.push(vscode.workspace.registerNotebookSerializer(
 			NOTEBOOK_TYPE, new CodexContentSerializer(), { transientOutputs: true }
 		),
 		new CodexKernel()
 	);
 
+	// Register extension commands
 	context.subscriptions.push(vscode.commands.registerCommand('codex-notebook-extension.createCodexNotebook', async () => {
 		vscode.window.showInformationMessage('Creating Codex Notebook');
 		const doc = await createCodexNotebook();
 		await vscode.window.showNotebookDocument(doc);
 	}));
 
-	context.subscriptions.push(vscode.commands.registerCommand('codex-notebook-extension.createCodexProject',
-		async () => {
-			vscode.window.showInformationMessage('Creating Codex Project');
-			await createProjectNotebooks();
-		}));
+	context.subscriptions.push(vscode.commands.registerCommand('codex-notebook-extension.createCodexProject', async () => {
+		vscode.window.showInformationMessage('Creating Codex Project');
+		await createProjectNotebooks();
+	}));
 
 }
