f68269aed4b6c8c3412cb3ebdf07688b0a6317a5
ryderwishart <ryderwishart@gmail.com>
2024-02-12 13:06:49 -0800

    Add new interface and component for comment text form

--- webviews/ChatSideBar/src/App.tsx
+++ webviews/ChatSideBar/src/App.tsx
@@ -1,9 +1,6 @@
 import { useState, useEffect } from "react";
-import {
-    VSCodeButton,
-    VSCodeTextField,
-    VSCodeTag,
-} from "@vscode/webview-ui-toolkit/react";
+import { VSCodeButton, VSCodeTag } from "@vscode/webview-ui-toolkit/react";
+import { CommentTextForm } from "../components/CommentTextForm";
 import "./App.css";
 import { ChatMessage, ChatPostMessages } from "../../../types";
 const vscode = acquireVsCodeApi();
@@ -12,6 +9,7 @@ const ChatRoleLabel = {
     system: "System",
     user: "You",
     assistant: "Copilot",
+    context: "Context",
 };
 
 interface MessageItemProps {
@@ -21,41 +19,60 @@ interface MessageItemProps {
 
 const MessageItem: React.FC<MessageItemProps> = ({
     messageItem,
-    showSenderRoleLabels = false,
+    showSenderRoleLabels = true,
 }) => {
     return (
         <>
-            <div
-                style={{
-                    fontSize: "0.8em",
-                    color: "lightgrey",
-                    marginBottom: "0.2em",
-                }}
-            >
-                {new Date().toLocaleTimeString()}{" "}
-                {/* FIXME: add actual timestamps */}
-            </div>
+            {(messageItem.role === "user" ||
+                messageItem.role === "assistant") && (
+                <div
+                    style={{
+                        fontSize: "0.8em",
+                        color: "lightgrey",
+                        marginBottom: "0.2em",
+                    }}
+                >
+                    {new Date().toLocaleTimeString()}{" "}
+                    {/* FIXME: add actual timestamps */}
+                </div>
+            )}
             <div
                 style={{
                     display: messageItem.role === "system" ? "none" : "flex",
                     flexDirection:
-                        messageItem.role === "user" ? "row" : "row-reverse",
+                        messageItem.role === "user"
+                            ? "row"
+                            : messageItem.role === "assistant"
+                              ? "row-reverse"
+                              : "column",
                     gap: "0.5em",
                     justifyContent:
-                        messageItem.role === "user" ? "flex-start" : "flex-end",
+                        messageItem.role === "user"
+                            ? "flex-start"
+                            : messageItem.role === "assistant"
+                              ? "flex-end"
+                              : "center",
                     borderRadius: "20px",
                     backgroundColor:
                         messageItem.role === "user"
                             ? "var(--vscode-editor-background)"
-                            : "var(--vscode-button-background)",
+                            : messageItem.role === "assistant"
+                              ? "var(--vscode-button-background)"
+                              : "lightblue", // distinct style for 'context' messages
                     color:
                         messageItem.role === "user"
                             ? "var(--vscode-editor-foreground)"
-                            : "var(--vscode-button-foreground)",
+                            : messageItem.role === "assistant"
+                              ? "var(--vscode-button-foreground)"
+                              : "black", // distinct style for 'context' messages
                     padding: "0.5em 1em",
-                    maxWidth: "80%",
+                    maxWidth: messageItem.role === "context" ? "100%" : "80%", // full width for 'context' messages
                     alignSelf:
-                        messageItem.role === "user" ? "flex-start" : "flex-end",
+                        messageItem.role === "user"
+                            ? "flex-start"
+                            : messageItem.role === "assistant"
+                              ? "flex-end"
+                              : "center",
                 }}
             >
                 {showSenderRoleLabels && (
@@ -77,6 +94,7 @@ function App() {
     const systemMessage: ChatMessage = {
         role: "system",
         content: "You are are helpful Bible translation assistant.",
+        // TODO: allow user to modify the system message
     };
     const dummyUserMessage: ChatMessage = {
         role: "user",
@@ -86,7 +104,7 @@ function App() {
         role: "assistant",
         content: "Let me check your current translation drafts...",
     };
-    const [pendingMessage, setMessage] = useState<ChatMessage>();
+    const [pendingMessage, setPendingMessage] = useState<ChatMessage>();
     const [messageLog, setMessageLog] = useState<ChatMessage[]>([
         systemMessage,
         dummyUserMessage,
@@ -105,7 +123,30 @@ function App() {
                     console.log({ state });
                     break;
                 }
-                // Handle other cases
+                case "select": {
+                    const { textDataWithContext } = message;
+                    const {
+                        selectedText,
+                        completeLineContent,
+                        vrefAtStartOfLine,
+                    } = textDataWithContext;
+                    console.log(`Selected text: ${selectedText}`);
+                    console.log(
+                        `Complete line content: ${completeLineContent}`,
+                    );
+                    console.log(
+                        `Verse reference at start of line: ${vrefAtStartOfLine}`,
+                    );
+
+                    const responseContent = textDataWithContext; // NOTE: this is an object... not a string. However, we want to render it not as a normal message but as a context display, sort of like a rendered code block
+
+                    // Update the pending message to show the assistant's response
+                    setPendingMessage({
+                        role: "context",
+                        content: responseContent,
+                    });
+                    break;
+                }
             }
         };
 
@@ -117,45 +158,83 @@ function App() {
         };
     }, []); // The empty array means this effect runs once on mount and cleanup on unmount
 
-    function handleClick() {
-        if (pendingMessage) {
-            const currentMessageLog = [...messageLog, pendingMessage];
-            setMessageLog(currentMessageLog);
-            // console.log({ currentMessageLog });
-            vscode.postMessage({
-                command: "fetch",
-                messages: JSON.stringify(currentMessageLog),
-            } as ChatPostMessages);
-            setMessage(undefined);
-        }
+    // function handleClick() {
+    //     if (pendingMessage) {
+    //         const currentMessageLog = [...messageLog, pendingMessage];
+    //         setMessageLog(currentMessageLog);
+    //         // console.log({ currentMessageLog });
+    //         vscode.postMessage({
+    //             command: "fetch",
+    //             messages: JSON.stringify(currentMessageLog),
+    //         } as ChatPostMessages);
+    //         setPendingMessage(undefined);
+    //     }
+    // }
+
+    function handleSubmit(submittedMessageValue: string) {
+        const pendingMessage: ChatMessage = {
+            role: "user",
+            content: submittedMessageValue,
+        };
+
+        const currentMessageLog = [...messageLog, pendingMessage];
+        setMessageLog(currentMessageLog);
+
+        vscode.postMessage({
+            command: "fetch",
+            messages: JSON.stringify(currentMessageLog),
+        } as ChatPostMessages);
+        // setPendingMessage(undefined);
     }
+
     // console.log("getState", vscode.getState());
     window.addEventListener(
         "message",
         (
             event: MessageEvent<{
-                command: "response";
+                command: "response" | "select";
                 finished: boolean;
                 text: string;
             }>,
         ) => {
             const messageInfo = event.data; // The JSON data our extension sent
-            console.log({ event, messageInfo, message: pendingMessage });
-            if (!event.data.finished) {
+            console.log("RYDER", {
+                event,
+                messageInfo,
+                message: pendingMessage,
+            });
+            if (messageInfo?.command === "select") {
+                console.log(
+                    "RYDER event.data.finished and pendingMessage.role === 'context'",
+                );
+                const messageContent = event.data.text;
+                setPendingMessage({
+                    role: "context",
+                    content: messageContent,
+                });
+            } else if (!event.data.finished) {
+                console.log("RYDER !event.data.finished");
                 const messageContent =
                     (pendingMessage?.content || "") + (event.data.text || "");
-                setMessage({
+                setPendingMessage({
                     role: "assistant",
                     content: messageContent,
                 });
-            } else {
+            } else if (event.data.finished) {
                 if (pendingMessage) {
                     setMessageLog([...messageLog, pendingMessage]);
                 }
-                setMessage(undefined);
+                setPendingMessage(undefined);
+            } else {
+                console.log("RYDER else");
+                if (pendingMessage) {
+                    setMessageLog([...messageLog, pendingMessage]);
+                }
+                setPendingMessage(undefined);
             }
         },
     );
+
     return (
         <main
             style={{
@@ -163,12 +242,29 @@ function App() {
                 flexDirection: "column",
                 height: "100vh",
                 width: "100%",
-                padding: "0",
                 backgroundImage: "linear-gradient(to bottom, #f5f5f5, #e0e0e0)", // FIXME: use vscode theme colors
                 backgroundSize: "cover",
                 overflowX: "hidden",
             }}
         >
+            <div
+                className="chat-header"
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "space-between",
+                    padding: "1em",
+                    borderBottom: "1px solid lightgrey",
+                }}
+            >
+                <h2 style={{ margin: 0 }}>Chat</h2>
+                <VSCodeButton
+                    aria-label="Clear"
+                    onClick={() => setMessageLog([systemMessage])}
+                >
+                    <i className="codicon codicon-trash"></i>
+                </VSCodeButton>
+            </div>
             <div
                 className="chat-container"
                 style={{
@@ -194,57 +290,8 @@ function App() {
                 )}
             </div>
             {/* Input for sending messages */}
-            <form
-                className="chat-input"
-                style={{
-                    position: "sticky",
-                    bottom: 0,
-                    width: "100%",
-                    display: "flex",
-                    gap: "0.25em",
-                    alignItems: "center",
-                    paddingInline: "0.5em",
-                    background: "var(--vscode-sideBar-background)",
-                }}
-                onSubmit={(e) => {
-                    e.preventDefault();
-                    handleClick();
-                }}
-            >
-                <VSCodeButton
-                    aria-label="Attach"
-                    onClick={() => console.log("Attach clicked")}
-                >
-                    <i className="codicon codicon-add"></i>
-                </VSCodeButton>
-                <VSCodeTextField
-                    placeholder="Type a message..."
-                    value={
-                        (pendingMessage?.role === "user" &&
-                            pendingMessage?.content) ||
-                        ""
-                    }
-                    onChange={(e) =>
-                        setMessage({
-                            role: "user",
-                            content: (e.target as HTMLInputElement).value,
-                        })
-                    }
-                    style={{
-                        flexGrow: 1,
-                        borderRadius: "5em",
-                    }}
-                />
-                <VSCodeButton type="submit">
-                    <i className="codicon codicon-send"></i>
-                </VSCodeButton>
-                <VSCodeButton
-                    aria-label="Record"
-                    onClick={() => console.log("Record clicked")}
-                >
-                    <i className="codicon codicon-mic"></i>
-                </VSCodeButton>
-            </form>
+
+            <CommentTextForm handleSubmit={handleSubmit} />
         </main>
     );
 }
