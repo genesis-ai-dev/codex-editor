37dd5611d700caea5de7aeb40086cbf92979a4e5
Ben Scholtens <ben.scholtens@me.com>
2024-02-12 17:06:12 -0500

    comments: partly finished ui

--- webviews/commentsWebview/src/components/CommentTextForm.tsx
+++ webviews/commentsWebview/src/components/CommentTextForm.tsx
@@ -20,6 +20,8 @@ export const CommentTextForm: React.FC<CommentTextFormProps> = ({
                 width: "100%",
                 display: "flex",
                 flexWrap: "nowrap",
+                boxShadow:
+                    "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
             }}
             onSubmit={(e) => {
                 e.preventDefault();
@@ -30,12 +32,44 @@ export const CommentTextForm: React.FC<CommentTextFormProps> = ({
                 (e.target as HTMLFormElement).reset();
             }}
         >
-            <VSCodeTextField
-                name="comment"
-                placeholder="Type a message..."
-                style={{ width: "100%" }}
-            />
-            <VSCodeButton type="submit">Save</VSCodeButton>
+            <div
+                style={{
+                    display: "flex",
+                    flexFlow: "column nowrap",
+                    padding: "0.5em 0",
+                    width: "100%",
+                }}
+            >
+                <label
+                    htmlFor="title"
+                    style={{ display: "block", marginBottom: "0.5em" }}
+                >
+                    Title:
+                </label>
+                <VSCodeTextField
+                    id="title"
+                    name="title"
+                    placeholder="Type the title..."
+                    style={{ width: "100%", marginBottom: "1em" }}
+                />
+            </div>
+            <div style={{ padding: "0.5em 0", width: "100%" }}>
+                <label
+                    htmlFor="comment"
+                    style={{ display: "block", marginBottom: "0.5em" }}
+                >
+                    Comment:
+                </label>
+                <VSCodeTextField
+                    id="comment"
+                    name="comment"
+                    placeholder="Type your comment..."
+                    style={{ width: "100%", marginBottom: "1em" }}
+                />
+            </div>
+            <VSCodeButton type="submit" style={{ padding: "0.5em" }}>
+                Save
+            </VSCodeButton>
         </form>
     );
 };
