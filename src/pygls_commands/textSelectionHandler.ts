e0bf10d235037ca1f68ff4fba45ee4bb17c80a7b
Daniel Losey <daniellosey@Daniels-MacBook-Pro.local>
2024-02-13 10:01:37 -0600

    removed notification

--- src/pygls_commands/textSelectionHandler.ts
+++ src/pygls_commands/textSelectionHandler.ts
@@ -12,7 +12,6 @@ async function checkServerHeartbeat() {
         if (data.databases === "") {
             const dataPath = vscode.workspace.workspaceFolders?.[0]?.uri.toString();
             if (dataPath) {
-                vscode.window.showInformationMessage('Server databases are empty. Attempting to start the server with data path: ' + dataPath);
                 await fetch(`http://localhost:5554/start?data_path=${encodeURIComponent(dataPath)}`, { method: 'GET' });
             } else {
                 console.error('No workspace folder found to start the server with.');
