61a5c7f9eccb0560646f94ae72dda274f6b9a78f
Ben Scholtens <ben.scholtens@me.com>
2024-02-13 15:59:33 -0500

    Feat: add comments react build as seperate vite build inside chat

--- webviews/ChatSideBar/components/CommentTextForm.tsx
+++ webviews/ChatSideBar/components/CommentTextForm.tsx
@@ -1,102 +1,86 @@
 import React from "react";
 import {
     VSCodeButton,
-    VSCodeTextArea,
     VSCodeTextField,
 } from "@vscode/webview-ui-toolkit/react";
-import "../src/App.css";
 
-type CommentTextFormProps = {
-    handleSubmit: (comment: string) => void;
-    contextItems: string[];
-    selectedText: string;
+export type CommentTextFormProps = {
+    handleSubmit: (args: {
+        comment: string;
+        title: string;
+        threadId: string | null;
+    }) => void;
+    showTitleInput?: boolean;
+    threadId: string | null;
 };
 
 export const CommentTextForm: React.FC<CommentTextFormProps> = ({
     handleSubmit,
-    contextItems,
-    selectedText,
+    showTitleInput,
+    threadId,
 }) => {
     return (
         <form
-            className="chat-input"
+            className="comments-input"
             style={{
                 position: "sticky",
                 bottom: 0,
                 width: "100%",
                 display: "flex",
-                flexDirection: "column",
-                gap: "0.25em",
-                alignItems: "center",
-                paddingInline: "0.5em",
-                background: "var(--vscode-sideBar-background)",
+                flexWrap: "nowrap",
+                boxShadow:
+                    "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
             }}
             onSubmit={(e) => {
                 e.preventDefault();
                 const formData = new FormData(e.target as HTMLFormElement);
-                const formValue = formData.get("chatInput") as string;
-                console.log("Form submitted with value:", formValue);
-                handleSubmit(formValue);
+                const comment = formData.get("comment") as string;
+                const title = formData.get("title") as string;
+
+                handleSubmit({ comment, title, threadId });
                 (e.target as HTMLFormElement).reset();
             }}
         >
-            <div
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    gap: "0.5em",
-                }}
-            >
-                {contextItems.length > 0 && (
-                    <VSCodeTextArea
-                        readOnly
-                        title="Context Items"
-                        value={contextItems?.join("\n")}
-                        placeholder="Context Items..."
-                        style={{ flexGrow: 1, marginBottom: "0.5em" }}
-                    />
-                )}
-                {selectedText && (
-                    <VSCodeTextArea
-                        readOnly
-                        title="Selected Text"
-                        value={selectedText}
-                        placeholder="Selected Text..."
-                        style={{ flexGrow: 1 }}
-                    />
-                )}
-            </div>
-            <div
-                style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    gap: "0.5em",
-                }}
-            >
-                <VSCodeButton
-                    aria-label="Attach"
-                    onClick={() => console.log("Attach clicked")}
-                >
-                    <i className="codicon codicon-add"></i>
-                </VSCodeButton>
-                <VSCodeTextField
-                    name="chatInput"
-                    placeholder="Type a message..."
+            {showTitleInput && (
+                <div
                     style={{
-                        flexGrow: 1,
-                        borderRadius: "5em",
+                        display: "flex",
+                        flexFlow: "column nowrap",
+                        padding: "0.5em 0",
+                        width: "100%",
                     }}
-                />
-                <VSCodeButton type="submit">
-                    <i className="codicon codicon-send"></i>
-                </VSCodeButton>
-                <VSCodeButton
-                    aria-label="Record"
-                    onClick={() => console.log("Record clicked")}
                 >
-                    <i className="codicon codicon-mic"></i>
-                </VSCodeButton>
+                    <label
+                        htmlFor="title"
+                        style={{ display: "block", marginBottom: "0.5em" }}
+                    >
+                        Title:
+                    </label>
+                    <VSCodeTextField
+                        id="title"
+                        name="title"
+                        placeholder="Type the title..."
+                        style={{ width: "100%", marginBottom: "1em" }}
+                    />
+                </div>
+            )}
+            <div style={{ padding: "0.5em 0", width: "100%" }}>
+                <label
+                    htmlFor="comment"
+                    style={{ display: "block", marginBottom: "0.5em" }}
+                >
+                    Comment:
+                </label>
+                <VSCodeTextField
+                    id="comment"
+                    name="comment"
+                    placeholder="Type your comment..."
+                    style={{ width: "100%", marginBottom: "1em" }}
+                />
             </div>
+            <VSCodeButton type="submit" style={{ padding: "0.5em" }}>
+                Save
+            </VSCodeButton>
         </form>
     );
 };
--- webviews/ChatSideBar/vite.config.ts
+++ webviews/ChatSideBar/vite.config.ts
@@ -1,17 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
+// Use an environment variable to specify the app to build
+const appToBuild = process.env.APP_NAME;
+
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "build",
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`,
-      },
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            input: `src/${appToBuild}/index.tsx`,
+            output: {
+                // Specify naming conventions here without a hash
+                entryFileNames: `[name].js`,
+                chunkFileNames: `[name].js`,
+                assetFileNames: `[name].[ext]`,
+            },
+        },
+        outDir: appToBuild ? `dist/${appToBuild}` : "dist",
     },
-  },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 });
