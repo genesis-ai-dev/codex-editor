b79eb11c26fceb892cf6b8cc9ad96bc665fe2c48
Ben Scholtens <ben.scholtens@me.com>
2024-02-14 17:11:05 -0500

    fix: fix chat request hang issue

--- webviews/codex-webviews/src/ChatView/index.tsx
+++ webviews/codex-webviews/src/ChatView/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./ChatView";
+import "../index.css";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
-    document.getElementById("root"),
 );
