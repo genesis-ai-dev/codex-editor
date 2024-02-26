dcd91ab4391480dd0345f26d689eb40886f8ed27
Kalisa <95277748+Kalisa11@users.noreply.github.com>
2024-02-26 08:55:48 +0200

    Add codicons stylesheet and implement resource sorting (#31)

--- webviews/obs/src/components/ResourcesTable.tsx
+++ webviews/obs/src/components/ResourcesTable.tsx
@@ -1,10 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useQuery } from "@tanstack/react-query";
-import fetchResource from "../utilities/fetchResources";
+import fetchResource, { resourceOrgIcons } from "../utilities/fetchResources";
 import { vscode } from "../utilities/vscode";
 import { useDownloadedResource } from "../hooks/useDownloadedResources";
 import { useState } from "react";
-import { VSCodeButton } from "@vscode/webview-ui-toolkit/react";
+import {
+    VSCodeButton,
+    VSCodeDropdown,
+    VSCodeOption,
+} from "@vscode/webview-ui-toolkit/react";
 import { TRANSLATION_RESOURCE_TYPES } from "../utilities/fetchTranslationResource";
 import { MessageType } from "../types";
 
@@ -31,6 +35,25 @@ const ResourcesTable = () => {
 
     const { downloadedResources } = useDownloadedResource();
 
+    const sortedResources = resources
+        ?.map((resource: any) => {
+            return {
+                ...resource,
+                isDownloaded: downloadedResources.some(
+                    (item) => item.id === resource.id,
+                ),
+            };
+        })
+        .sort((a: any, b: any) => {
+            // Sort downloaded resources first
+            if (a.isDownloaded && !b.isDownloaded) {
+                return -1;
+            } else if (!a.isDownloaded && b.isDownloaded) {
+                return 1;
+            }
+            return 0;
+        });
+
     const handleDownload = (resource: any) => {
         console.log(resource);
         vscode.postMessage({
@@ -56,23 +79,32 @@ const ResourcesTable = () => {
         });
     };
 
-    console.log("resources", resources?.[0]);
+    const handleOrgImage = (organization: string) => {
+        let icon = "";
+        resourceOrgIcons.forEach((org) => {
+            if (org.org === organization) {
+                icon = org.icon;
+            }
+        });
+        return icon;
+    };
 
     return (
         <div>
-            <div className="flex justify-center gap-3">
-                Main Resources
-                {RESOURCE_TYPES.map((type) => (
-                    <VSCodeButton onClick={() => setResourceType(type.key)}>
-                        {type.label}
-                    </VSCodeButton>
-                ))}
+            <div className="flex justify-between w-full">
+                Filter Resources
+                <VSCodeDropdown className="w-1/2">
+                    {RESOURCE_TYPES.map((type) => (
+                        <VSCodeOption onClick={() => setResourceType(type.key)}>
+                            {type.label}
+                        </VSCodeOption>
+                    ))}
+                </VSCodeDropdown>
             </div>
-            <table className="table-auto">
-                <thead>
+            <table className="table-auto w-full">
+                <thead className="font-semibold">
                     <tr>
                         <td>Resource</td>
-                        <td>Type</td>
                         <td>Organization</td>
                         <td>Version</td>
                         <td></td>
@@ -80,19 +112,34 @@ const ResourcesTable = () => {
                 </thead>
 
                 <tbody className="gap-3">
-                    {resources?.map((resource: any) => (
+                    {sortedResources?.map((resource: any) => (
                         <tr>
-                            <td>{resource.name}</td>
-                            <td>{resource.subject}</td>
-                            <td>{resource.owner}</td>
-                            <td>{`${resource.released.split("T")[0]} (${
-                                resource.release.tag_name
-                            })`}</td>
+                            <td>{resource.language_title}</td>
+
                             <td>
+                                {handleOrgImage(resource.owner) !== "" ? (
+                                    <img
+                                        src={handleOrgImage(resource.owner)}
+                                        alt={resource.owner}
+                                        className="w-8 h-8 rounded-lg object-contain"
+                                    />
+                                ) : (
+                                    resource.owner
+                                )}
+                            </td>
+                            <td
+                                title={`Released on : ${new Date(resource.released).toLocaleDateString()}`}
+                            >
+                                {resource.release.tag_name}
+                            </td>
+                            <td className="flex items-center justify-center px-2">
                                 {downloadedResources.find(
                                     (item) => item.id === resource.id,
                                 ) ? (
-                                    <button
+                                    <VSCodeButton
+                                        title="Open Resource"
+                                        appearance="primary"
+                                        className="w-full"
                                         onClick={() =>
                                             handleOpenResource(
                                                 downloadedResources.find(
@@ -102,14 +149,17 @@ const ResourcesTable = () => {
                                             )
                                         }
                                     >
-                                        Open
-                                    </button>
+                                        <i className="codicon codicon-eye"></i>
+                                    </VSCodeButton>
                                 ) : (
-                                    <button
+                                    <VSCodeButton
+                                        title="Download Resource"
+                                        appearance="secondary"
+                                        className="w-full"
                                         onClick={() => handleDownload(resource)}
                                     >
-                                        Download
-                                    </button>
+                                        <i className="codicon codicon-cloud-download"></i>
+                                    </VSCodeButton>
                                 )}
                             </td>
                         </tr>
