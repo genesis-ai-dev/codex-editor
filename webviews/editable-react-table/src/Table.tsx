14464598d21202805a9b531bd5490e5ca42f54d6
Ben Scholtens <ben.scholtens@me.com>
2024-02-26 16:43:30 -0500

    add types and fix some styles

--- webviews/editable-react-table/src/Table.tsx
+++ webviews/editable-react-table/src/Table.tsx
@@ -1,10 +1,11 @@
-import React, { useMemo } from 'react';
+import React, { CSSProperties, useMemo } from 'react';
 import clsx from 'clsx';
 import {
   useTable,
   useBlockLayout,
   useResizeColumns,
   useSortBy,
+  TableOptions,
 } from 'react-table';
 import Cell from './cells/Cell';
 import Header from './header/Header';
@@ -12,7 +13,9 @@ import PlusIcon from './img/Plus';
 import { ActionTypes } from './utils';
 import { FixedSizeList } from 'react-window';
 import scrollbarWidth from './scrollbarWidth';
-
+interface CustomTableOptions<T extends object> extends TableOptions<T> {
+  dataDispatch?: React.Dispatch<any>; // Adjust the type according to your dispatch function
+}
 const defaultColumn: TableColumn = {
   minWidth: 50,
   width: 150,
@@ -28,11 +31,16 @@ export default function Table({
   dispatch: dataDispatch,
   skipReset,
 }: TableData) {
-// export const Table: React.FC<TableData> = ({ columns, data, dispatch: dataDispatch, skipReset }) => {
+  // export const Table: React.FC<TableData> = ({ columns, data, dispatch: dataDispatch, skipReset }) => {
 
   const sortTypes = useMemo(
     () => ({
-      alphanumericFalsyLast(rowA: any, rowB: any, columnId: string, desc?: boolean) {
+      alphanumericFalsyLast(
+        rowA: any,
+        rowB: any,
+        columnId: string,
+        desc?: boolean
+      ) {
         if (!rowA.values[columnId] && !rowB.values[columnId]) {
           return 0;
         }
@@ -60,26 +68,23 @@ export default function Table({
     rows,
     prepareRow,
     totalColumnsWidth,
-  } = useTable(
+  } = useTable<TableEntry>(
     {
       columns,
       data,
       defaultColumn,
-      dataDispatch,
+      dataDispatch, // Now correctly recognized as part of the configuration
       autoResetSortBy: !skipReset,
       autoResetFilters: !skipReset,
       autoResetRowState: !skipReset,
       sortTypes,
-    },
+    } as CustomTableOptions<TableEntry>, // Cast to your custom interface
     useBlockLayout,
     useResizeColumns,
     useSortBy
   );
-
   const RenderRow = React.useCallback(
-    
     ({ index, style }: { index: number; style: React.CSSProperties }) => {
-
       const row = rows[index];
       prepareRow(row);
       return (
@@ -95,28 +100,39 @@ export default function Table({
     [prepareRow, rows]
   );
 
-  function isTableResizing(): boolean {
-    for (let headerGroup of headerGroups) {
-      for (let column of headerGroup.headers) {
-        if (column.isResizing) {
-          return true;
-        }
-      }
-    }
+  // function isTableResizing(): boolean {
+  //   for (let headerGroup of headerGroups) {
+  //     for (let column of headerGroup.headers) {
+  //       if (column.isResizing) {
+  //         return true;
+  //       }
+  //     }
+  //   }
 
-    return false;
-  }
+  //   return false;
+  // }
+
+  const Rows: React.FC = () => (
+    <div>
+      {rows.map((row, index: number) =>
+        RenderRow({ index, style: row.getRowProps().style as CSSProperties })
+      )}
+    </div>
+  );
 
   return (
     <div style={{ maxWidth: '100vw', overflow: 'auto' }}>
-      <div
-        {...getTableProps()}
-        className={clsx('table', isTableResizing() && 'noselect')}
-      >
+      <div {...getTableProps()} className={'table'}>
         <div>
           {headerGroups.map((headerGroup: any, index: number) => (
-            <div {...headerGroup.getHeaderGroupProps()} key={index} className="tr">
-              {headerGroup.headers.map((column: any) => column.render('Header', { key: column.id}))}
+            <div
+              {...headerGroup.getHeaderGroupProps()}
+              key={index}
+              className="tr"
+            >
+              {headerGroup.headers.map((column: any) =>
+                column.render('Header', { key: column.id })
+              )}
             </div>
           ))}
         </div>
@@ -125,13 +141,16 @@ export default function Table({
             height={480}
             itemCount={rows.length}
             itemSize={40}
-            width={totalColumnsWidth + scrollbarWidth}
+            // width={totalColumnsWidth + scrollbarWidth}
           >
             {RenderRow}
           </FixedSizeList>
+          <Rows />
           <div
             className="tr add-row"
-            onClick={() => dataDispatch && dataDispatch({ type: ActionTypes.ADD_ROW })}
+            onClick={() =>
+              dataDispatch && dataDispatch({ type: ActionTypes.ADD_ROW })
+            }
           >
             <span className="svg-icon svg-gray icon-margin">
               <PlusIcon />
