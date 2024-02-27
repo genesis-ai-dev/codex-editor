b21c3812549325e26615284388985806835c22c1
dadukhankevin <danieljlosey@gmail.com>
2024-02-26 19:49:32 -0600

    Parallel passages now includes anomalies, databases are offloaded when not used, and the genetic tokenizer is included

--- servers/server.py
+++ servers/server.py
@@ -5,13 +5,20 @@ import socket
 from typing import NoReturn
 
 try:
+    import sys # TODO: See if this takes too much time
+
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    requirements_file = os.path.join(script_directory, "requirements.txt")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--break-system-packages", "-q", "-r", requirements_file])
+
+
     from pygls.server import LanguageServer
     from tools.ls_tools import ServerFunctions
     from servable.spelling import ServableSpelling
     from servable.servable_wb import wb_line_diagnostic
     from servable.verse_validator import ServableVrefs
     from servable.servable_embedding import ServableEmbedding
-
+    import genetok
     import flask # forces install if it is not installed
     import flask_cors # forces install if it is not installed
     import sys
@@ -19,7 +26,7 @@ except ImportError:
     import sys
     script_directory = os.path.dirname(os.path.abspath(__file__))
     requirements_file = os.path.join(script_directory, "requirements.txt")
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "--break-system-packages", "-r", requirements_file])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--break-system-packages", "-q", "-r", requirements_file])
     exit()
 
 
@@ -66,6 +73,7 @@ def start_flask_server() -> None:
     flask_server_path = os.path.join(os.path.dirname(__file__), "flask_server.py")
     with open(os.devnull, 'w') as devnull:
         subprocess.Popen([sys.executable, flask_server_path], stdout=devnull, stderr=devnull)
+        
 threading.Thread(target=start_flask_server, daemon=True).start()
 
 # Initialize the language server with metadata
