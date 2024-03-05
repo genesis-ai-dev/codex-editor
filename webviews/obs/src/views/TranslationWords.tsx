ffad51a5bbc956e5afa900b682d0b4045d7e75cd
Brian Ineza <brianineza01@gmail.com>
2024-03-05 13:38:23 +0100

    ft: resources rendering tw

---
+++ webviews/obs/src/views/TranslationWords.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { renderToPage } from "../utilities/main-vscode";
+import { vscode } from "../utilities/vscode";
+import { TranslationWord } from "../types";
+import TranslationWordsDropdown from "../components/TranslationWordsDropdown";
+import TranslationWordRenderer from "../components/TranslationWordRenderer";
+
+const TranslationWords = () => {
+    const [translationWord, setTranslationWord] =
+        useState<TranslationWord | null>(null);
+    useEffect(() => {
+        vscode.setMessageListeners();
+    }, []);
+    return (
+        <div className="flex flex-col">
+            <TranslationWordsDropdown setTranslationWord={setTranslationWord} />
+            <TranslationWordRenderer translationWord={translationWord} />
+        </div>
+    );
+};
+
+renderToPage(<TranslationWords />);
