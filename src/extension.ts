b68fc4bb64ac16b8bdc0e2bc667f79a938b395a9
Ben Scholtens <ben.scholtens@me.com>
2024-03-12 15:18:31 -0400

    refactor: move comments and chat into their own extension

--- src/extension.ts
+++ src/extension.ts
@@ -72,7 +72,6 @@ import {
     integer,
 } from "vscode-languageclient/node";
 import { registerCommentsProvider } from "./commentsProvider";
-import { registerChatProvider } from "./providers/chat/customChatWebviewProvider";
 import { registerCommentsWebviewProvider } from "./providers/commentsWebview/customCommentsWebviewProvider";
 import { registerParallelViewWebviewProvider } from "./providers/parallelPassagesWebview/customParallelPassagesWebviewProvider";
 import { registerDictionaryTableProvider } from "./providers/dictionaryTable/dictionaryTableProvider";
@@ -96,9 +95,6 @@ const ROOT_PATH = getWorkSpaceFolder();
 
 const PATHS_TO_POPULATE = [
     // "metadata.json", // This is where we store the project metadata in scripture burrito format, but we create this using the project initialization command
-    { filePath: "comments.json", defaultContent: "" }, // This is where we store the VS Code comments api comments, such as on .bible files
-    { filePath: "notebook-comments.json", defaultContent: "[]" }, // We can't use the VS Code comments api for notebooks (.codex files), so a second files avoids overwriting conflicts
-    { filePath: "chat-threads.json", defaultContent: "[]" }, // This is where chat thread conversations are saved
     { filePath: "drafts/" }, // This is where we store the project drafts, including project.dictionary and embedding dbs
     { filePath: "drafts/target/" }, // This is where we store the drafted scripture in particular as .codex files
     { filePath: "drafts/project.dictionary", defaultContent: "" }, // This is where we store the project dictionary
@@ -628,9 +624,6 @@ export async function activate(context: vscode.ExtensionContext) {
     isExtensionInitialized = true;
     registerReferencesCodeLens(context);
     registerSourceCodeLens(context);
-    registerCommentsProvider(context);
-    registerChatProvider(context);
-    registerCommentsWebviewProvider(context);
     registerParallelViewWebviewProvider(context);
     registerDictionaryTableProvider(context);
     registerDictionarySummaryProvider(context);
