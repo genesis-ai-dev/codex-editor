0edc562c7a63c8f49632448fbcf67b89412c2f03
Ben Scholtens <ben.scholtens@me.com>
2024-03-19 11:28:16 -0400

    update index when new source bible is added

--- src/extension.ts
+++ src/extension.ts
@@ -589,6 +589,7 @@ export async function activate(context: vscode.ExtensionContext) {
                     }
                 }
             }
+            indexVerseRefsInSourceText();
         },
     );
 
