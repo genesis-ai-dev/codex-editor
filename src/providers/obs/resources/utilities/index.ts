4eb040fb050a112044f5ea60ca1093ee6896b0fe
Brian Ineza <brianineza01@gmail.com>
2024-04-03 07:50:04 +0200

    ft: obs resources download

--- src/providers/obs/resources/utilities/index.ts
+++ src/providers/obs/resources/utilities/index.ts
@@ -7,9 +7,17 @@ export const getResourceType = (subject: string) => {
     if (subject === "TSV Translation Questions") return "tq";
     if (subject === "Translation Words") return "tw";
     if (subject === "TSV Translation Words Links") return "twl";
-    if (subject === "OBS Translation Notes") return "obs-tn";
+    if (
+        subject === "OBS Translation Notes" ||
+        subject === "TSV OBS Translation Notes"
+    )
+        return "obs-tn";
     if (subject === "OBS Translation Questions") return "obs-tq";
     if (subject === "TSV OBS Translation Questions") return "obs-tq";
-    if (subject === "OBS Translation Words Links") return "obs-twl";
+    if (
+        subject === "OBS Translation Words Links" ||
+        subject === "TSV OBS Translation Words Links"
+    )
+        return "obs-twl";
     throw new Error("Invalid resource type");
 };
