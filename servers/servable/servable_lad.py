4fa95ae041d5025360f125a612a304bace0723ea
dadukhankevin <danieljlosey@gmail.com>
2024-04-17 12:10:43 -0500

    vaguely better lad

--- servers/servable/servable_lad.py
+++ servers/servable/servable_lad.py
@@ -1,16 +1,11 @@
-"""
-LAD
-"""
-import requests
+import re
 from typing import List
 from lsprotocol.types import Diagnostic, DocumentDiagnosticParams, Position, Range, DiagnosticSeverity
-import math
+import requests
 
-last_diagnostics: List[Diagnostic] = []
-
-def get_lad_score(line):
+def get_lad_score(verse, vref):
     try:
-        response = requests.get("http://localhost:5554/line_lad?query="+line, timeout=1)
+        response = requests.get(f"http://localhost:5554/line_lad?query={verse}&ref={vref}", timeout=1)
         if response.status_code == 200:
             score = response.json()['score']
             return float(score)
@@ -22,21 +17,45 @@ def get_lad_score(line):
 def lad_diagnostic(ls, params: DocumentDiagnosticParams, sf):
     """
     LAD diagnostic
-    """    
-
+    """
     diagnostics: List[Diagnostic] = []
     document_uri = params.text_document.uri
     if ".codex" in document_uri or ".scripture" in document_uri:
         document = ls.workspace.get_document(document_uri)
-    
+
     lines = document.lines
+    verse_pattern = re.compile(r'(\w{2,3})\s(\d+):(\d+)')
+    verse = ""
+    verse_start = None
+    verse_end = None
+    vref = ""
+
     for line_num, line in enumerate(lines):
-        if len(line) > 12:
-            data = get_lad_score(line)
-            if data:
-                score = round(data, 2)
-                if score and score < 39:
-                    range_ = Range(start=Position(line=line_num, character=0),
-                                end=Position(line=line_num, character=len(line)))
-                    diagnostics.append(Diagnostic(range=range_, message=f"Source and target have low overlap: {score}", severity=DiagnosticSeverity.Warning, source='Anomaly Detection'))
+        matches = verse_pattern.finditer(line)
+        for match in matches:
+            if verse:
+                data = get_lad_score(verse, vref)
+                if data:
+                    score = round(data, 2)
+                    if score and score < 39:
+                        range_ = Range(start=verse_start, end=verse_end)
+                        diagnostics.append(Diagnostic(range=range_, message=f"Source and target have low overlap: {score}", severity=DiagnosticSeverity.Warning, source='Anomaly Detection'))
+            
+            verse = match.group()
+            vref = f"{match.group(1)} {match.group(2)}:{match.group(3)}"
+            verse_start = Position(line=line_num, character=match.start())
+            verse_end = Position(line=line_num, character=match.end())
+        
+        if verse:
+            verse += ' ' + line[verse_end.character:].strip()
+            verse_end = Position(line=line_num, character=len(line))
+
+    if verse:
+        data = get_lad_score(verse, vref)
+        if data:
+            score = round(data, 2)
+            if score and score < 39:
+                range_ = Range(start=verse_start, end=verse_end)
+                diagnostics.append(Diagnostic(range=range_, message=f"Source and target have low overlap: {score}", severity=DiagnosticSeverity.Warning, source='Anomaly Detection'))
+
     return diagnostics
