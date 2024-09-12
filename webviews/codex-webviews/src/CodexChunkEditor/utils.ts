f12a4275c21ae8af72cf59d90f682990c6b05001
ryderwishart <ryderwishart@gmail.com>
2024-09-12 09:33:40 -0600

    Bump version; Fix some type errors; add a note

--- webviews/codex-webviews/src/CodexChunkEditor/utils.ts
+++ webviews/codex-webviews/src/CodexChunkEditor/utils.ts
@@ -1,7 +1,7 @@
 export const processVerseContent = (cellContent: string) => {
     const verseRefRegex = /(?<=^|\s)(?=[A-Z, 1-9]{3} \d{1,3}:\d{1,3})/;
     const lines = cellContent.split(verseRefRegex);
-    
+
     return lines
         .map((line) => {
             const verseMarker = line.match(/(\b[A-Z, 1-9]{3}\s\d+:\d+\b)/)?.[0];
@@ -20,6 +20,19 @@ export const processVerseContent = (cellContent: string) => {
         .filter((line) => line !== null);
 };
 
+/**
+ * HACKY_removeContiguousSpans is a function that removes contiguous spans from a given HTML string.
+ * It uses a regular expression to find and replace all occurrences of </span><span> with an empty string,
+ * effectively removing the contiguous spans, and 'merging' the text back together into the same span.
+ * 
+ * The reason we needed this is because when you add a <li></li> to a verse, it will not render as a block
+ * inside a span, so we need to end the current span whenever you insert a <li>. However, if the user
+ * later removes the <li>, you will suddenly have two contiguous spans where there should only be one. The 
+ * solution is to remove all contiguous spans to handle this edge case.
+ * 
+ * @param {string} html - The HTML string from which contiguous spans need to be removed.
+ * @returns {string} - The HTML string with contiguous spans removed.
+ */
 export const HACKY_removeContiguousSpans = (html: string) => {
     return html.replace(/<\/span><span>/g, '');
 };
