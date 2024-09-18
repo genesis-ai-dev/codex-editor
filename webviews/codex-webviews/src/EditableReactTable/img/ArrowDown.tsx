6c8495cb451aa1f24cf4aa4af68e31876d2fc4dc
ryderwishart <ryderwishart@gmail.com>
2024-09-18 14:51:08 -0600

    Use codicons in dictionary editor

--- webviews/codex-webviews/src/EditableReactTable/img/ArrowDown.tsx
+++ webviews/codex-webviews/src/EditableReactTable/img/ArrowDown.tsx
@@ -1,23 +1,7 @@
 import React from 'react';
 
-const Arrow: React.FC = () => {
-  return (
-    <svg
-      width="44"
-      height="44"
-      viewBox="0 0 24 24"
-      strokeWidth="1.5"
-      stroke="#2c3e50"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <line x1="12" y1="5" x2="12" y2="19" />
-      <line x1="18" y1="13" x2="12" y2="19" />
-      <line x1="6" y1="13" x2="12" y2="19" />
-    </svg>
-  );
+const ArrowDown: React.FC = () => {
+  return <span className="codicon codicon-chevron-down"></span>;
 };
 
-export default Arrow;
+export default ArrowDown;
--- webviews/codex-webviews/src/EditableReactTable/img/ArrowLeft.tsx
+++ webviews/codex-webviews/src/EditableReactTable/img/ArrowLeft.tsx
@@ -1,23 +1,7 @@
 import React from 'react';
 
-const Arrow: React.FC = () => {
-  return (
-    <svg
-      width="44"
-      height="44"
-      viewBox="0 0 24 24"
-      strokeWidth="1.5"
-      stroke="#2c3e50"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <line x1="5" y1="12" x2="19" y2="12" />
-      <line x1="5" y1="12" x2="11" y2="18" />
-      <line x1="5" y1="12" x2="11" y2="6" />
-    </svg>
-  );
+const ArrowLeft: React.FC = () => {
+  return <span className="codicon codicon-chevron-left"></span>;
 };
 
-export default Arrow;
+export default ArrowLeft;
