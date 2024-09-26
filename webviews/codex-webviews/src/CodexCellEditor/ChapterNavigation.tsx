1118d00ccba57b8e0254ea55e689fd180c811429
ryderwishart <ryderwishart@gmail.com>
2024-09-26 05:23:44 -0600

    Clean up chapter header in cell editor

--- webviews/codex-webviews/src/CodexCellEditor/ChapterNavigation.tsx
+++ webviews/codex-webviews/src/CodexCellEditor/ChapterNavigation.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { VSCodeButton } from "@vscode/webview-ui-toolkit/react";
 
 interface ChapterNavigationProps {
     chapterNumber: number;
     setChapterNumber: React.Dispatch<React.SetStateAction<number>>;
     scriptureCellsLength: number;
     unsavedChanges: boolean;
+    onAutocompleteChapter: () => void;
 }
 
 const ChapterNavigation: React.FC<ChapterNavigationProps> = ({
@@ -12,20 +14,33 @@ const ChapterNavigation: React.FC<ChapterNavigationProps> = ({
     setChapterNumber,
     scriptureCellsLength,
     unsavedChanges,
+    onAutocompleteChapter,
 }) => (
     <div className="chapter-navigation">
-        <button
+        <VSCodeButton
+            appearance="icon"
             disabled={chapterNumber === 0 || unsavedChanges}
             onClick={() => setChapterNumber(chapterNumber - 1)}
         >
             <i className="codicon codicon-chevron-left"></i>
-        </button>
-        <button
+        </VSCodeButton>
+        <div className="chapter-navigation-group">
+            <VSCodeButton
+                appearance="icon"
+                onClick={onAutocompleteChapter}
+                disabled={unsavedChanges}
+                title="Autocomplete Chapter"
+            >
+                <i className="codicon codicon-sparkle"></i>
+            </VSCodeButton>
+        </div>
+        <VSCodeButton
+            appearance="icon"
             disabled={chapterNumber === scriptureCellsLength - 1 || unsavedChanges}
             onClick={() => setChapterNumber(chapterNumber + 1)}
         >
             <i className="codicon codicon-chevron-right"></i>
-        </button>
+        </VSCodeButton>
     </div>
 );
 
