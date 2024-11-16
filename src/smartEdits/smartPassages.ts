1b8d2551876d3baaefc6a75e1259135d3c5bfc0f
dadukhankevin <danieljlosey@gmail.com>
2024-11-15 17:22:17 -0700

    codex help

--- src/smartEdits/smartPassages.ts
+++ src/smartEdits/smartPassages.ts
@@ -3,13 +3,25 @@ import * as path from "path";
 import Chatbot from "./chat";
 import { TranslationPair, MinimalCellResult } from "../../types";
 
+const GENERAL_CODEX_HELP = `
+Codex is an AI-assisted text translation (usually Bible) tool for translators built as a set of extensions on top of sodium/Visual Studio Code. If the user asks for help, it may be something that your general knowledge of the app may solve. The individuals you are speaking with may have very little technical literacy, so make sure to be very clear and err on the side of over-explaining things. Now, Codex projects have source files “.source” and Codex files “.codex”. “.codex” files are the files the translators edit, while “.source” files contain the content they are translating from. 
+There are several WebViews that the translators use:
+The typical ones associated with Visual Studio Code
+- Codex Resource Explorer Allows for translators to download and explore various translation resources. These include: Translation Notes, Translation Words List, Translation Academy (helps translators learn how to translate), and Translation Words.
+- Navigation A neat UI for selecting and opening specific Bible passages.
+- Parallel Passages A tool to search the translation, as well as talk to an LLM about specific passages. (This is where you are!) They can search key words/phrases, or in the main editor they can click the ‘Pin” icon, and it will show up as a search result here. There are two tabs: “Search” and “Chat”. In Chat they can speak with the Assistant (you) about their progress in translating these things.
+- Project Manager Here, users can create new projects or edit important settings for their current ones. They can also change their source/target languages or download/import various source language Bibles. This can also solve problems where they may notice certain parts of the app generating content in the wrong language.
+- Comments This allows for translators to add comments to verses, as a way to communicate with each other about the project.
+`;
+
 const SYSTEM_MESSAGE = `You are a helpful assistant translation assistant.
 You will be given large amounts of parallel texts between two languages.
 Your job is to help the user understand the texts and make sense of them.
 You will also be given historical edits of the texts, and other relevant information.
 - If the user asks for the original language, you should respond with the original language of the text within a codeblock for clarity.
-- If the user asks for technical help with "codex", respond that you are not capable of that yet, but you will be able to soon. Feel free to attempt to help them with other technical questions.
 - Steer the user towards translating texts in culturally appropriate ways, focus on maintaining the meaning of the text.
+Here is some information about the app that the user is using:
+${GENERAL_CODEX_HELP}
 Always respond in markdown format.
 `;
 
