ff0c6d56c7b3cd0fc627552ca992c3d87911d2c4
TimRl <tim.rahhal@gmail.com>
2025-01-22 15:15:34 -0700

    Removed unnecessary `&nbsp;` and `&nbsp; ` entities from the .codex files.

--- webviews/codex-webviews/src/CodexCellEditor/Editor.tsx
+++ webviews/codex-webviews/src/CodexCellEditor/Editor.tsx
@@ -133,8 +133,8 @@ export default function Editor(props: EditorProps) {
                 setUnsavedChanges(true);
                 return delta;
             });
-            
 
+            
             quillRef.current = quill;
 
             if (props.initialValue) {
@@ -171,13 +171,14 @@ export default function Editor(props: EditorProps) {
                     if (props.onChange) {
                         const cleanedContents = getCleanedHtml(content);
 
-                        // New function to remove excessive empty paragraphs and line breaks
+                        // New function to remove excessive empty paragraphs and line breaks and &nbsp; (of both kinds) which appear inconsistently
                         const removeExcessiveEmptyTags = (html: string) => {
                             return html
                                 .replace(/<p><br><\/p>/g, "<p></p>") // Replace <p><br></p> with <p></p>
                                 .replace(/<p><\/p>(\s*<p><\/p>)+/g, "<p></p>") // Remove consecutive empty paragraphs
                                 .replace(/^(\s*<p><\/p>)+/, "") // Remove leading empty paragraphs
-                                .replace(/(\s*<p><\/p>)+$/, ""); // Remove trailing empty paragraphs
+                                .replace(/(\s*<p><\/p>)+$/, "") // Remove trailing empty paragraphs
+                                .replace(/&nbsp; ?/g, "");
                         };
 
                         const trimmedContent = removeExcessiveEmptyTags(cleanedContents);
