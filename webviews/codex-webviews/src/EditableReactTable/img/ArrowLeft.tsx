73c484c6cf9dbe3c28063511b28e8a339da93881
dadukhankevin <danieljlosey@gmail.com>
2026-02-04 12:52:23 -0600

    large deletions

--- webviews/codex-webviews/src/EditableReactTable/img/ArrowLeft.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ArrowLeft: React.FC = () => {
-    return <span className="codicon codicon-chevron-left"></span>;
-};
-
-export default ArrowLeft;
--- webviews/codex-webviews/src/EditableReactTable/img/Text.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Text: React.FC = () => {
-    return <span className="codicon codicon-symbol-string"></span>;
-};
-
-export default Text;
